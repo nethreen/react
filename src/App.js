fedd1c27cdb46529c7273ffd44d62fab55abff2c
Soltanli Nazrin Rovshen  <Sola_uy20@itstep.edu.az>
2019-04-17 11:45:04 +0400

    new

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import One from './componets/one'
+import './componets/test.scss'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+           <h3>App</h3>
+           <One/>
       </div>
     );
   }
 }
 
 export default App;
+
+
+
---
+++ src/componets/one.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+ class One extends Component {
+  render() {
+    return (
+      <div>
+        <from className="from">
+        <input className="inp" type="dfasjfg"/>
+        <button>Gonder</button>
+        </from>
+        
+      </div>
+    )
+  }
+}
+export default One;
